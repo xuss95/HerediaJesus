3cc739f1e900147f2c64a4c301d5488b8a1441f1
Jesús Heredia <jesushema95@gmail.com>
2026-05-21 16:06:22 +0200

    Mi primer commit de index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// Fichero principal del proyecto
+$nombre = "Jesús Heredia";
+$curso = "DAW - Despliegue de Aplicaciones Web";
+
+echo "<h1>Bienvenido, " . $nombre . "</h1>";
+echo "<p>Curso: " . $curso . "</p>";
+?>
